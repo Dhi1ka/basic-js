6549d8b2ca2a7b3be0f183455348d315af6f46d6
dhi1ka <daw.knight04@gmail.com>
2021-12-28 14:07:26 +0700

    tambah algoritma perhitungan

---
+++ algoritma-pseudocode/index.js
@@ -0,0 +1,10 @@
+// var panjang = 10;
+// var lebar = 5;
+// var luas = panjang * lebar;
+// console.log(luas);
+
+// var r = 7;
+// var pi = 3.14;
+// var volume = 4/3 * pi * r * r * r;
+
+// console.log(volume);
